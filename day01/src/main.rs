e0194df667a2ab3d7527d7985b34156b2bba43dd
k0pernicus <antonin.carette@gmail.com>
2017-12-06 19:48:12 +0100

    Update the code for the second part of the exercise

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,51 +2,22 @@ use std::env;
 use std::io::{self, Write};
 use std::process;
 
-trait CharConversion {
-    fn parse(&self) -> Option<u32>;
-}
-
-impl CharConversion for char {
-    fn parse(&self) -> Option<u32> {
-        self.to_digit(10)
-    }
-}
-
-pub fn parse(input: &str) -> Option<u32> {
+pub fn parse(input: &str, offset: usize) -> Option<u32> {
+    let input_len = input.len();
     // Check if the string is not empty
-    if input.len() == 0 {
-        return None
+    if input_len == 0 || offset >= input_len {
+        return None;
     }
     // Contains the "list" of numbers
-    let mut vector_of_number: Vec<u32> = Vec::new();
-    // Take the last character of the string reference, and add it to the vector of numbers
-    let mut last_character = match input.chars().last().unwrap().parse() {
-        Some(value) => value,
-        // If the last character can't be parse to u32 type, exit
-        None => {
-            writeln!(io::stdout(), "Error when parsing the last character of the string").unwrap();
-            return None
-        } 
-    };
-    // Convert each character to number
-    for (index, character) in input.chars().enumerate() {
-        match character.parse() {
-            Some(value) => {
-                // If the new character is the same than the last one, add it to the vector of numbers
-                if value == last_character {
-                    vector_of_number.push(value);
-                }
-                last_character = value;
-            }
-            // If the character is not a number, exit
-            None => {
-                writeln!(io::stdout(), "Error when parsing the string: canno't convert char to usize at position {} for char '{}", index, character).unwrap();
-                return None
-            }
+    let vector_of_number = input.chars().map(|x| x.to_digit(10).expect(&format!("Character {} can be parsed as u32 type", x))).collect::<Vec<u32>>();
+    let mut sum_vector = 0;
+    for (index, number) in vector_of_number.iter().enumerate() {
+        let compare_number = vector_of_number[(index + offset) % input_len];
+        if *number == compare_number {
+            sum_vector += *number;
         }
     }
-    // Return the sum
-    Some(vector_of_number.iter().sum())
+    Some(sum_vector)
 }
 
 fn main() {
@@ -61,9 +32,11 @@ fn main() {
     }
 
     let input = args.last().unwrap();
+    let input = input.as_str();
+    let offset: usize = input.len() / 2;
 
     // Return the result
-    match parse(input.as_str()) {
+    match parse(input, offset) {
         Some(value) => println!("{}", value),
         None => process::exit(1),
     }
@@ -78,42 +51,79 @@ mod test {
 
     #[test]
     fn test_wrong_input_1() {
-        assert_eq!(None, parse(""));
+        assert_eq!(None, parse("", 1));
     }
 
     #[test]
+    #[should_panic]
     fn test_wrong_input_2() {
-        assert_eq!(None, parse("1234h"));
+        assert_eq!(None, parse("1234h", 1));
     }
 
     #[test]
+    #[should_panic]
     fn test_wrong_input_3() {
-        assert_eq!(None, parse(" 1234  2"));
+        assert_eq!(None, parse(" 1234  2", 1));
     }
 
     #[test]
+    #[should_panic]
     fn test_wrong_input_4() {
-        assert_eq!(None, parse("1helloworld0"));
+        assert_eq!(None, parse("1helloworld0", 1));
     }
 
     #[test]
     fn test_1122() {
-        assert_eq!(Some(3), parse("1122"));
+        assert_eq!(Some(3), parse("1122", 1));
+        assert_eq!(Some(0), parse("1122", 2));
+    }
+
+    #[test]
+    fn test_1212() {
+        assert_eq!(Some(0), parse("1212", 1));
+        assert_eq!(Some(6), parse("1212", 2));
+    }
+
+    #[test]
+    fn test_1221() {
+        assert_eq!(Some(3), parse("1221", 1));
+        assert_eq!(Some(0), parse("1221", 2));
     }
 
     #[test]
     fn test_1111() {
-        assert_eq!(Some(4), parse("1111"));
+        assert_eq!(Some(4), parse("1111", 1));
+        assert_eq!(Some(4), parse("1111", 2));
     }
 
     #[test]
     fn test_1234() {
-        assert_eq!(Some(0), parse("1234"));
+        assert_eq!(Some(0), parse("1234", 1));
+        assert_eq!(Some(0), parse("1234", 2));
+    }
+
+    #[test]
+    fn test_123425() {
+        assert_eq!(Some(0), parse("123425", 1));
+        assert_eq!(Some(4), parse("123425", 3));
+    }
+
+    #[test]
+    fn test_123123() {
+        assert_eq!(Some(0), parse("123123", 1));
+        assert_eq!(Some(12), parse("123123", 3));
+    }
+
+    #[test]
+    fn test_12131415() {
+        assert_eq!(Some(0), parse("12131415", 1));
+        assert_eq!(Some(4), parse("12131415", 4));
     }
 
     #[test]
     fn test_91212129() {
-        assert_eq!(Some(9), parse("91212129"));
+        assert_eq!(Some(9), parse("91212129", 1));
+        assert_eq!(Some(6), parse("91212129", 4));
     }
 
 }
